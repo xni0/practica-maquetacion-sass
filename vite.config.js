1b665fb3469f28695c2a730e69fa6d893c78c692
xni0 <alumno.206012@ies-azarquiel.es>
2025-11-23 06:52:29 +0100

    Configuración final para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,23 @@
 import { defineConfig } from "vite";
-import { resolve } from "path"; // Necesario para resolver las rutas de entrada
+import { resolve } from "path"; 
 
 export default defineConfig({
-  // Indica que la raíz del proyecto para el desarrollo es la carpeta 'src'
+
   root: "./src", 
   
-  // La ruta base debe ser relativa ('./') para que funcione correctamente
-  // cuando el proyecto se sirve desde un subdirectorio en GitHub Pages.
-  base: "./", 
+
+  base: "/practica-maquetacion-sass/", 
 
   build: {
-    // Especifica la carpeta de salida (docs) para GitHub Pages
-    outDir: "../docs", 
+ 
+    outDir: "../dist",
+    emptyOutDir: true, 
     
     rollupOptions: {
       input: {
-        // Definimos todas las entradas HTML. 
-        // Es crucial incluir el path completo desde la raíz del proyecto
-        main: resolve(__dirname, "src/index.html"),    // Página principal
-        about: resolve(__dirname, "src/about.html"),   // Página 'Sobre mí'
-        contact: resolve(__dirname, "src/contact.html"), // Página 'Contacto'
+        main: resolve(__dirname, "src/index.html"),
+        about: resolve(__dirname, "src/about.html"),
+        contact: resolve(__dirname, "src/contact.html"),
       },
     },
   },
